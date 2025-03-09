08b7f84e52e708d2ba5cc24fec6539f41c9fbfe2
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2025-03-09 19:36:06 +0100

    feat: update alias and move dev dir to personal

--- scripts/.config/scripts/open_dir.sh
+++ scripts/.config/scripts/open_dir.sh
@@ -2,12 +2,12 @@
 
 # Search for directories using fzf
 directories="$@"
-selected_dir=$(find ${directories[@]} -maxdepth 1 -type d -not -path '*/.*' 2>/dev/null | sort | fzf)
+selected_dir=$(find ${directories[@]} -mindepth 1 -maxdepth 1 -type d -not -path '*/.*' 2>/dev/null | sort | fzf --delimiter="/" --nth=-1)
 
 # Check if a directory was selected
-if [[ ! -n "$selected_dir" ]]; then
+if [[ -z "$selected_dir" ]]; then
     pwd
     exit 1
 fi
 
-echo $selected_dir
+echo "$selected_dir"
